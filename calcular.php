64de1a3b6cfdf5241afaae9340e9ce04bc40cc0e
Roberto Cortes <robertocortes291402@gmail.com>
2022-06-30 15:05:09 -0500

    Se modifican los codesmells

--- calcular.php
+++ calcular.php
@@ -3,19 +3,23 @@ $cantidad = $_POST['cantidad'];
 $subtotal = $cantidad*125;
 $numero_cajas = ceil($cantidad/4);
 $flete = $numero_cajas*50;
-if ($cantidad>1000)
+if ($cantidad>1000){
     $descuento=$subtotal*0.15;
+}
 else
 {
-    if($cantidad>100)
+    if($cantidad>100){
         $descuento=$subtotal*0.05;
-    else
+    } 
+    else {
         $descuento=0;
+    }
 }
 $total=$subtotal+$flete-$descuento;
 echo "El precio total a pagar es: ".$total."<br>";
 echo "El monto por flete es: ".$flete."<br>";
 echo "El descuento aplicado fue: ".$descuento;
+
 $edad = 24;
 switch($edad)
 {
@@ -28,6 +32,9 @@ switch($edad)
     case 48:
         echo "El doble de tu edad";
         break;
+    default:
+        echo "Se hizo un echo"
+    break;
 }
 
 ?>
